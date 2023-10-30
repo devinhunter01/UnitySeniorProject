b0c529ef0635100aeb281cf5093ccd6d77350f8f
JoseTongo01 <jose.tongo01@utrgv.edu>
2023-10-29 23:34:54 -0500

    Created multiplayer

---
+++ Assets/JT_MuliplayerTest/RoomManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    public GameObject player;
+
+    [Space]
+    public Transform spawnPoint;
+    
+    void Start()
+    {
+        Debug.Log("Connecting....");
+        PhotonNetwork.ConnectUsingSettings();
+        
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        Debug.Log("Connected to Server!");
+        PhotonNetwork.JoinLobby();
+    }
+    
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        Debug.Log("We are in a lobby");
+        PhotonNetwork.JoinOrCreateRoom("test",null,null);
+        
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        Debug.Log("We are connected and in a room!");
+        GameObject _player = PhotonNetwork.Instantiate(player.name, spawnPoint.position, Quaternion.identity);
+        _player.GetComponent<PlayerSetup>().IsLocalPlayer();
+    }
+   
+}
+
